342c72986686744d23c0f6fb999cf6d71273b3c8
harshit joshi <harshjoshi25022002@gmail.com>
2026-03-12 21:46:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-5xl text-emerald-500  underline font-semibold absolute top-1/2 left-1/2 translate-0.5'>Hello </h1>
+    </div>
+  )
+}
+
+export default App
